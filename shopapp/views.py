02236910d27df2e7afdbdfc911e6602eaa75dd8b
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-10-25 13:22:07 +0500

    create navbar in base.html

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'shopapp/home.html')
+
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'shopapp/about.html')
