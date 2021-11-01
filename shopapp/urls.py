b22917a964f51ad93835658f8c2858442966ca7b
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-11-01 10:45:47 +0500

    Create account form

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+
     path('cart/', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', views.remove_cart, name='remove_cart'),
     path('cart/remove_product/<int:product_id>', views.remove_product, name='remove_product'),
+
+    path('account/create/', views.sing_up_view, name='signup'),
 ]
